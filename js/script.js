be42dde7ac04409317455f342e049e181d2c0166
Muhammad Faatih <mhmmdfaatih@gmail.com>
2024-02-28 20:44:13 +0700

    fix minor bug and waiting review (if lucky :D)

--- js/script.js
+++ js/script.js
@@ -13,23 +13,25 @@ function validateForm() {
     const birthDate = document.forms["message-form"]["birth-date"].value;
     const gender = document.forms["message-form"]["gender"].value;
     const messages = document.forms["message-form"]["messages"].value;
+    const date = Date();
 
     if(name == "" || birthDate == "" || gender == "" || messages == ""){
-        alert("Tidak boleh ada yang kosong");
+        alert("Periksa kembali inputan anda, pastikan semua data sudah diisi...");
         //document.getElementById("error-name").innerHTML = "Tidak boleh kosong!";
         return false;
     }
 
-    setSenderUI(name, birthDate, gender, messages);
+    setSenderUI(name, birthDate, gender, messages, date);
 
     return false;
 }
 
-function setSenderUI(name, birthDate, gender, messages) {
+function setSenderUI(name, birthDate, gender, messages, date) {
     document.getElementById("sender-full-name").innerHTML = name;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
     document.getElementById("sender-gender").innerHTML = gender;
     document.getElementById("sender-messages").innerHTML = messages;
+    document.getElementById("current-time").innerHTML = date;
 
     alert("Terima kasih atas saran dan masukannya :)\n\n " + 
     "\"Kami tidak pernah meragukan pelanggan \n " +  
